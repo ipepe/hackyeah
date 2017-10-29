d47a936f92525f9833a083fd31120513a45417a4
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2017-10-29 04:38:43 +0100

    Fixed cleaing addresses

--- app/models/teryt_location.rb
+++ app/models/teryt_location.rb
@@ -6,9 +6,16 @@ class TerytLocation < ApplicationRecord
 
   def self.clean(address)
     result = address
-    %w[aleja ulica osiedle plac rondo al ul os pl].each do |c|
+    %w[aleja ulica osiedle plac rondo].each do |c|
       result = result.gsub(c, '')
     end
-    result.parameterize.gsub('-', ' ')
+    result = result.parameterize.gsub(/[-\/.]/, ' ')
+    %w[al ul os pl].each do |c|
+      if result.start_with?(c+' ')
+        result = result[3..-1]
+      end
+      result.gsub(/\A#{c} /, '')
+    end
+    result
   end
 end
